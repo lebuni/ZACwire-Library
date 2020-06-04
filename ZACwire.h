3aa47b3b6a611ef21d0effeb86419650c5f1a5b6
Adrian Immer <62163284+lebuni@users.noreply.github.com>
2020-06-04 23:16:43 +0200

    added comments

--- ZACwire.h
+++ ZACwire.h
@@ -17,11 +17,11 @@ class ZACwire {
   		_Sensortype = Sensortype;
   	}
 	
-	bool begin() {			//start reading sensor, needs to be called 100ms before the first getTemp()
+	bool begin() {			//start collecting data, needs to be called 100ms before the first getTemp()
 	  pinMode(pin, INPUT);
-	  bitWindow = 125;		//bitWindow is twice the Tstrobe
+	  bitWindow = 125;		//change from 0 to 125 to give the getTemp the info begin() was already executed
 	  deltaTime = micros();
-	  if (!pulseInLong(pin, LOW)) return false;
+	  if (!pulseInLong(pin, LOW)) return false;	//check if there is an incoming signal
 	  isrPin = digitalPinToInterrupt(pin);
 	  if (isrPin == -1) return false;
 	  attachInterrupt(isrPin, read, RISING);
@@ -36,26 +36,26 @@ class ZACwire {
 			begin();
 			delay(110);
 		}
-		noInterrupts();  				//no ISRs because tempValue might change during reading
-		if (BitCounter != 20) misreading = true;
+		noInterrupts();  				//no ISRs because tempValue might fluctuate
+		if (BitCounter != 20) misreading = true;	//use misreading-backup when newer reading is incomplete
 		else newBitWindow = ((ByteTime << 5) + (ByteTime << 4) + ByteTime >> 9) + 20;	//+20 found out by trial and error
 		uint16_t tempHigh = tempValue[0][backUP^misreading];		//get high significant bits from ISR
-		uint16_t tempLow = tempValue[1][backUP^misreading];		//get low significant bits from ISR
+		uint16_t tempLow = tempValue[1][backUP^misreading];		//get low   ''		''
 		interrupts();
 		if (abs(bitWindow-newBitWindow) < 20) bitWindow += (newBitWindow >> 3) - (bitWindow >> 3);	//adjust bitWindow time, which varies with rising temperature
 		for (byte i = 0; i < 9; ++i) {
-		  if (tempHigh & 1 << i) ++parity1;
+		  if (tempHigh & 1 << i) ++parity1;	//count "1" bits, which have to be even --> failure check
 		  if (tempLow & 1 << i) ++parity2;
 		}
 		if (tempHigh | tempLow && ~(parity1 | parity2) & 1) {       // check for failure
-			tempHigh >>= 1;               	// delete parity bits
+			tempHigh >>= 1;               		// delete parity bits
 			tempLow >>= 1;
-			tempLow |= tempHigh << 8;		//joints high and low significant figures
+			tempLow |= tempHigh << 8;		//join high and low significant figures
 			misreading = false;
 			if (_Sensortype < 400) return (float(tempLow * 250L >> 8) - 499) / 10;	//calculates °C
 			else return (float(tempLow * 175L >> 9) - 99) / 10;
 		}
-		else if (!misreading) {
+		else if (!misreading) {		//restart with backUP raw temperature
 			misreading = true;
 			getTemp();
 		}
@@ -65,7 +65,7 @@ class ZACwire {
 		}
   	}
   
-  	void end() {			//stop reading
+  	void end() {
   		detachInterrupt(isrPin);
   	}
 
@@ -76,7 +76,7 @@ class ZACwire {
 		unsigned long microtime = micros();
   		deltaTime = microtime - deltaTime;	//measure time to previous rising edge
   		if (deltaTime > 1000) {		  	//true at start bit
-  			ByteTime = microtime;			//for measuring Tstrobe/bitWindow
+  			ByteTime = microtime;		//for measuring Tstrobe/bitWindow
 			backUP = !backUP;
   			BitCounter = ByteNr = tempValue[0][backUP] = tempValue[1][backUP] = 0;
   		}
@@ -91,7 +91,7 @@ class ZACwire {
   	}
 	
   	int isrPin;
-  	int _Sensortype;					//either 206, 306 or 506
+  	int _Sensortype;		//either 206, 306 or 506
   	static volatile byte BitCounter;
   	static volatile unsigned long ByteTime;
   	static volatile uint16_t tempValue[2][2];
