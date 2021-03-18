ec942fac88aa1ce986a46d5c8035cf1ec55cbc64
Adrian Immer <62163284+lebuni@users.noreply.github.com>
2021-03-18 13:13:35 +0100

    Added connection check
    added wire connection check before every output: `return 221` if not signal detected
    replaced configurable offset with fixed 15, which was tested on ESP32, ESP8266, Arduino Uno and Arduino Mega 2560
    changed defaultBitWindow to 135
    simplified code for adjusting the bitWindow

--- ZACwire.h
+++ ZACwire.h
@@ -1,6 +1,6 @@
 /*	ZACwire - Library for reading temperature sensors TSIC 206/306/506
 	created by Adrian Immer in 2020
-	v1.2.4b
+	v1.2.5b
 */
 
 #ifndef ZACwire_h
@@ -13,14 +13,13 @@ class ZACwire {
 	
   public:
   
-  	ZACwire(int Sensortype = 306, byte defaultBitWindow = 130, byte offset = 10, bool core = 0){
+  	ZACwire(int Sensortype = 306, byte defaultBitWindow = 135, bool core = 0){
   		_Sensortype = Sensortype;
-		_defaultBitWindow = defaultBitWindow;	//expected BitWindow in µs, depends on sensor
-		_offset = offset;			//offset for new calculated BitWindow in µs
+		_defaultBitWindow = defaultBitWindow;	//expected BitWindow in µs, depends on sensor & temperature
 		_core = core;				//only ESP32: choose cpu0 or cpu1
   	}
 	
-	bool begin() {					//start collecting data, needs to be called 100ms before the first getTemp()
+	bool begin() {					//start collecting data, needs to be called 100+ms before the first getTemp()
 	  pinMode(pin, INPUT);
 	  bitWindow = _defaultBitWindow;		//change from 0 to defaultBitWindow to give the getTemp the info begin() was already executed
 	  deltaTime = micros();
@@ -35,25 +34,32 @@ class ZACwire {
 	  return true;
   	}
   
-	float getTemp() {	    	//gives back temperature in °C
+	float getTemp() {	    			//gives back temperature in °C
 		static bool misreading = false;
 		static byte newBitWindow;
 		byte parity1 = 0, parity2 = 0;
-		if (!bitWindow) {	//check if begin() was already called
+		if (micros() - deltaTime >> 17) {    	//check wire connection for the last 131ms
+			if (!bitWindow) { 		//check if begin() was already called
 			begin();
 			delay(110);
+			}
+			else return 221;		// temp=221 if sensor not connected
 		}
-		if (BitCounter != 20) misreading = true;	//use misreading-backup when newer reading is incomplete
-		else newBitWindow = ((ByteTime << 5) + (ByteTime << 4) + ByteTime >> 9) + _offset;	//divide by 10.5 and add offset
+		if (BitCounter == 20) newBitWindow = ((ByteTime << 5) + (ByteTime << 4) + ByteTime >> 9) + 15;	//divide by 10.5 and add offset
+		else misreading = !misreading;					//use misreading-backup when newer reading is incomplete
 		uint16_t tempHigh = rawTemp[0][backUP^misreading];		//get high significant bits from ISR
 		uint16_t tempLow = rawTemp[1][backUP^misreading];		//get low   ''		''
-		if (abs(bitWindow-newBitWindow) < 20) bitWindow += (newBitWindow >> 3) - (bitWindow >> 3);	//adjust bitWindow time, which varies with temperature
+		
+		if (bitWindow == 135) bitWindow = newBitWindow;			//adjust bitWindow time, which varies with temperature
+		else if (bitWindow < newBitWindow) ++bitWindow;
+		else --bitWindow;
+		
 		for (byte i = 0; i < 9; ++i) {
 		  if (tempHigh & 1 << i) ++parity1;	//count "1" bits, which have to be even --> failure check
 		  if (tempLow & 1 << i) ++parity2;
 		}
-		if (tempHigh | tempLow && ~(parity1 | parity2) & 1) {       // check for failure
-			tempHigh >>= 1;               		// delete parity bits
+		if (tempHigh | tempLow && ~(parity1 | parity2) & 1) {		// check for failure
+			tempHigh >>= 1;               				// delete parity bits
 			tempLow >>= 1;
 			tempLow |= tempHigh << 8;		//join high and low significant figures
 			misreading = false;
@@ -66,7 +72,7 @@ class ZACwire {
 		}
 		else {
 			misreading = false;
-			return 222;		//set to 222 if reading failed
+			return 222;		// temp=222 if reading failed
 		}
   	}
   
@@ -77,7 +83,7 @@ class ZACwire {
   private:	
 	
 	#ifdef ESP32
-	static void attachISR_ESP32(void *arg){			//attach ISR in freeRTOS because arduino can't attachInterrupt() inside of template clas
+	static void attachISR_ESP32(void *arg){			//attach ISR in freeRTOS because arduino can't attachInterrupt() inside of template class
 	  gpio_pad_select_gpio((gpio_num_t)isrPin);
 	  gpio_set_direction((gpio_num_t)isrPin, GPIO_MODE_INPUT);
 	  gpio_set_intr_type((gpio_num_t)isrPin, GPIO_INTR_POSEDGE);
@@ -105,19 +111,18 @@ class ZACwire {
 		}
 		rawTemp[ByteNr][backUP] <<= 1;
 		if (deltaTime > bitWindow);		//Logic 0
-		else if (deltaTime < bitWindow - 40 || rawTemp[ByteNr][backUP] & 2) rawTemp[ByteNr][backUP] |= 1;	//Logic 1
+		else if (deltaTime < bitWindow - 30 || rawTemp[ByteNr][backUP] & 2) rawTemp[ByteNr][backUP] |= 1;	//Logic 1
   		deltaTime = microtime;
   	}
 	
   	static int isrPin;
   	int _Sensortype;		//either 206, 306 or 506
 	byte _defaultBitWindow;		//expected BitWindow in µs, according to datasheet 125
-	byte _offset;
 	bool _core;
   	static volatile byte BitCounter;
   	static volatile unsigned long ByteTime;
   	static volatile uint16_t rawTemp[2][2];
-  	static unsigned long deltaTime;
+  	static volatile unsigned long deltaTime;
   	static byte bitWindow;
   	static volatile bool backUP;
 };
@@ -131,7 +136,7 @@ volatile bool ZACwire<pin>::backUP;
 template<uint8_t pin>
 volatile uint16_t ZACwire<pin>::rawTemp[2][2];
 template<uint8_t pin>
-unsigned long ZACwire<pin>::deltaTime;
+volatile unsigned long ZACwire<pin>::deltaTime;
 template<uint8_t pin>
 int ZACwire<pin>::isrPin;
 template<uint8_t pin>
