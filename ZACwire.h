5e157cf0d60ca7bfecd5c70e4fe7b4deda9168b9
Adrian Immer <62163284+lebuni@users.noreply.github.com>
2022-01-27 14:20:09 +0100

    Upload

--- ZACwire.h
+++ ZACwire.h
@@ -1,6 +1,6 @@
 /*	ZACwire - Library for reading temperature sensors TSIC 206/306/506
 	created by Adrian Immer in 2020
-	v2.0.0
+	14bit_v1.0.0
 */
 
 #ifndef ZACwire_h
@@ -17,11 +17,11 @@
 
 class ZACwire {
 	public:
-		explicit ZACwire(uint8_t inputPin, int16_t sensor=306);
+		explicit ZACwire(uint8_t inputPin);
 		
 		bool begin();					//start reading
 		
-		float getTemp(uint8_t maxChangeRate=10, bool useBackup=false);	//return temperature in °C
+		uint16_t getRawVal(bool useBackup=false);	//return temperature in °C
 		
 		void end();
 		
@@ -29,7 +29,7 @@ class ZACwire {
 		const uint8_t timeout		{100};		//timeout in ms to give error 221
 		const uint8_t errorNotConnected	{221};
 		const uint8_t errorMisreading	{222};
-		enum Bit {lastZero=5, afterStop=10, finished=19};
+		enum Bit {lastZero=2, afterStop=10, finished=19};
 		
 		static void IRAM_ATTR isrHandler(void* ptr);
 		
@@ -37,14 +37,10 @@ class ZACwire {
 		
 		bool connectionCheck();				//check heartbeat of the ISR
 		
-		void adjustBitThreshold();			//improve the accuracy of the bitThreshold over time
-
-		bool tempCheck(uint16_t rawTemp);		//validate the received temperature
+		bool parityCheck(uint16_t rawVal);		//validate the received temperature
 
 		uint8_t _pin;
-		int16_t _sensor;
 		uint32_t timeLastHB{};				//timestamp, when ISR stopped sending heartbeats
-		int16_t prevTemp{};				//to calculate the change rate
 
 		STATIC uint16_t measuredTimeDiff;		//measured time between rising edges of signal
 		STATIC uint8_t bitThreshold;
